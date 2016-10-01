c8cf6e209d5216e5571a86346cb47a82e777d7ac
Jeff Zotz <zotzjm@gmail.com>
2016-09-30 23:05:44 -0400

    HEY I FIXED THE NAME

---
+++ MicroRemote/MicroRemote/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MicroRemote
+//
+//  Created by Jeff Zotz on 9/30/16.
+//  Copyright © 2016 Jeff Zotz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
