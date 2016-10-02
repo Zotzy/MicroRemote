758c074b279127270170505401bdbb01b87550ab
Reece Boyd <rab7r@mtmail.mtsu.edu>
2016-10-02 07:41:43 -0400

    added that code back in

--- MicroRemote/MicroRemote/HttpWrapper.swift
+++ MicroRemote/MicroRemote/HttpWrapper.swift
@@ -60,21 +60,21 @@ class HttpWrapper {
 		
 		request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
 		
-//		let key = KeychainSwift()
-//		
-//		if let pass = key.get("password") {
-//			let dataString = String(data: request.httpBody!, encoding: .utf8)
-//			let encryptedJSON = AES256CBC.encryptString(dataString!, password: pass)
-//			let encryptedJSONData = encryptedJSON?.data(using: .utf8)
-//			request.httpBody = encryptedJSONData
-//		} else {
-//			let pass = AES256CBC.generatePassword()
-//			key.set(pass, forKey: "password")
-//			let dataString = String(data: request.httpBody!, encoding: .utf8)
-//			let encryptedJSON = AES256CBC.encryptString(dataString!, password: pass)
-//			let encryptedJSONData = encryptedJSON?.data(using: .utf8)
-//			request.httpBody = encryptedJSONData
-//		}
+		let key = KeychainSwift()
+		
+		if let pass = key.get("password") {
+			let dataString = String(data: request.httpBody!, encoding: .utf8)
+			let encryptedJSON = AES256CBC.encryptString(dataString!, password: pass)
+			let encryptedJSONData = encryptedJSON?.data(using: .utf8)
+			request.httpBody = encryptedJSONData
+		} else {
+			let pass = AES256CBC.generatePassword()
+			key.set(pass, forKey: "password")
+			let dataString = String(data: request.httpBody!, encoding: .utf8)
+			let encryptedJSON = AES256CBC.encryptString(dataString!, password: pass)
+			let encryptedJSONData = encryptedJSON?.data(using: .utf8)
+			request.httpBody = encryptedJSONData
+		}
 
 		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 		request.addValue("application/json", forHTTPHeaderField: "Accept")
